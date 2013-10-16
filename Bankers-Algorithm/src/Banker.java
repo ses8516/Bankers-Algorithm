defdf7fca016efad8e6a1a4a4bc5c4be661ea7a4
ses8516 <ses8516@trumpet.honors.gccis.rit.edu>
2013-10-16 19:47:44 -0400

    Driver class mostly done. Not sure about last part with join().
    Banker class still needs a lot of work

--- Bankers-Algorithm/src/Banker.java
+++ Bankers-Algorithm/src/Banker.java
@@ -6,51 +6,91 @@
 
 public class Banker {
 
-public volatile int allocUnits = 0;
-public volatile int availUnits = 0;
+	private volatile int allocUnits;
+	private volatile int availUnits;
+	
+	private Thread claim = null; 
+	
+	/**
+	* Main constructor for Banker class
+	* @param nUnits - # of units from the system the Banker wants to obtain initially
+	*/
+	public Banker(int nUnits){
+		allocUnits = nUnits;
+		availUnits = nUnits;
+	}
+	
+	/**
+	* Attempt to claim more units from the system, if they are available.
+	* @param nUnits
+	*/
+	public void setClaim(int nUnits){
+		if(claim != null || nUnits < 1 || nUnits > availUnits){
+			System.exit(1);
+		}
 
-/**
-* Main constructor for Banker class
-* @param nUnits - # of units from the system the Banker wants to obtain initially
-*/
-public Banker(int nUnits){
-
-}
-
-/**
-* Attempt to claim more units from the system, if they are available.
-* @param nUnits
-*/
-public void setClaim(int nUnits){
-if(nUnits < 0){
-
-}
-//if(nUnits > System units)
-//if(this.hasMadeClaim)
-//Same return as the first one.
-}
-
-
-/**
-* Attempt to claim more units.
-*/
-public boolean request(int nUnits){
-
-}
-
-public void release(int nUnits){
-
-}
-
-/**
-* Returns no
-*/
-public int allocated( ){
-return allocUnits;
-}
-
-    public int remaining( ){
-     return availUnits;
-    }
-
+		claim = Thread.currentThread();
+		
+		System.out.println("Thread "+Thread.currentThread().getName()+" sets a claim for "+nUnits+" units.");
+	}
+	
+	
+	/**
+	* Attempt to claim more units.
+	*/
+	public boolean request(int nUnits){
+		System.out.println("Thread "+Thread.currentThread().getName()+" requests "+nUnits+" units.");
+		
+		if (safe()){
+			
+		}else{
+			while(true){
+				System.out.println("Thread "+Thread.currentThread().getName()+" waits.");
+				try {
+					Thread.currentThread().wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Thread "+Thread.currentThread().getName()+" awakened.");
+				
+				if (safe()) {
+					System.out.println("Thread "+Thread.currentThread().getName()+" has "+nUnits+" units allocated.");
+					allocUnits += nUnits;
+					availUnits += nUnits;
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Determine if state is safe
+	 */
+	private boolean safe(){
+		return false;
+	}
+	
+	public void release(int nUnits){
+		if( claim != Thread.currentThread() || nUnits > 0 || nUnits > allocUnits){
+			System.exit(1);
+		}
+		System.out.println("Thread "+Thread.currentThread().getName()+" releases "+nUnits+" units.");
+		allocUnits -= nUnits;
+		Thread.currentThread().notifyAll();
+	}
+	
+	/**
+	* @return number of allocated units
+	*/
+	public int allocated( ){
+		return allocUnits;
+	}
+	
+	/**
+	 * @return number of available units
+	 */
+	public int remaining( ){
+		return availUnits;
+	}
 }
--- Bankers-Algorithm/src/Driver.java
+++ Bankers-Algorithm/src/Driver.java
@@ -1,7 +1,42 @@
+import java.util.ArrayList;
+
 
 public class Driver {
-
+	// Number of Clients
+	private final static int NUM_CLIENTS = 5;
+	
+	// Number of units to give the banker
+	private final static int BANKER_UNITS = 5;
+	
+	// Number of units each clients will request
+	private final static int CLIENT_UNITS = 5;
+	
+	// Number of request/release loops for each client to loop through
+	private final static int NUM_REQUESTS = 5;
+	
+	// Minimum time in milliseconds for each client to sleep between requests
+	private final static long MIN_SLEEP_TIME = 1000;
+	
+	// Maximum time in milliseconds for each client to sleep between requests
+	private final static long MAX_SLEEP_TIME = 5000;
+	
 	public static void main(String[] args){
+		Banker banker = new Banker(BANKER_UNITS);
 		
+		ArrayList<Client> clients = new ArrayList<Client>(NUM_CLIENTS);
+		
+		for(int i = 0; i < NUM_CLIENTS; i++){
+			clients.add( new Client(""+i,banker,CLIENT_UNITS,NUM_REQUESTS,MIN_SLEEP_TIME,MAX_SLEEP_TIME));
+		}
+		
+		for (Client c: clients){
+			c.start();
+		}
+		
+		try {
+			Thread.currentThread().join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
