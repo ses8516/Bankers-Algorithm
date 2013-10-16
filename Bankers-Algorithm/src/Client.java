959fff66ad40e1c5b0bf2ecbd966ababc4d925ba
ses8516 <ses8516@trumpet.honors.gccis.rit.edu>
2013-10-16 19:05:50 -0400

    Client class is done. Could use comments though

--- Bankers-Algorithm/src/Client.java
+++ Bankers-Algorithm/src/Client.java
@@ -1,4 +1,35 @@
-
 public class Client extends Thread {
-
+	private final Banker banker;
+	private final int nUnits;
+	private final int nRequests;
+	private final long minSleepMillis;
+	private final long maxSleepMillis;
+	
+	public Client(String name, Banker banker, int nUnits, int nRequests, long minSleepMillis, long maxSleepMillis){
+		super(name);
+		this.banker = banker;
+		this.nUnits = nUnits;
+		this.nRequests = nRequests;
+		this.minSleepMillis = minSleepMillis;
+		this.maxSleepMillis = maxSleepMillis;
+	}
+	
+	public void run(){
+		banker.setClaim(nUnits);
+		for(int i = 0; i < nRequests; i++){
+			if(banker.remaining() == 0){
+				banker.release(nUnits);
+			}else{
+				banker.request(nUnits);
+			}
+			
+			try {
+				sleep( (long) ((Math.random() * (maxSleepMillis - minSleepMillis )) + minSleepMillis) );
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		banker.release(nUnits);
+	}
 }
